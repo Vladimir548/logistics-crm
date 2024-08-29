8f29e6ecdc24d712d19311ab7383692a30598a44
Vladimir548 <kormisenkovvladimir@gmail.com>
2024-08-29 14:52:21 +0400

    add wrapper suspense

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -1,12 +1,13 @@
 import Application from "@/app/application/_ui/Application";
 import HomeLayout from "@/app/layouts/HomeLayout";
+import {Suspense} from "react";
 
 export default function Page() {
     return (
-
+<Suspense fallback={null}>
             <HomeLayout scroll={false}>
                 <Application/>
             </HomeLayout>
-
+</Suspense>
     );
 };
